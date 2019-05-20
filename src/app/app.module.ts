cbb0ba0e1718a323740e1342b7e6d6ecc1d11fd0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-20 23:44:30 +0000

    Deploy Kayle7777/angularCart to github.com/Kayle7777/angularCart.git:gh-pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { BillingFormComponent } from './billing-form/billing-form.component';
+import { HowdyComponent } from './howdy/howdy.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [NgbModule, BrowserModule],
+  declarations: [AppComponent, BillingFormComponent, HowdyComponent, FooterComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
